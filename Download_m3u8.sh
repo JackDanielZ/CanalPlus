89b82df6f305a809a559ff6933edb9f2d5b7248a
Daniel Zaoui <daniel.zaoui@yahoo.com>
2015-05-20 18:21:18 +0300

    Im[rove time slots download (process and display)

--- Download_m3u8.sh
+++ Download_m3u8.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # $1: link to m3u8
-#$2 output file
+# $2 output file
 # Download the m3u8 file, search for ts files, wget and combine them 
 
-rm -f ts_1*
-wget -T 30 -t 10 -q -O file.m3u8 "$1"
+rm -f /tmp/ts_out $2
+wget -T 30 -t 10 -q -O /tmp/file.m3u8 "$1"
 
-ts_files="`grep "\.ts" file.m3u8`"
-index=0
+ts_files="`grep "\.ts" /tmp/file.m3u8`"
+nb_files=`echo "$ts_files" | wc -l`
+index=1
 for ts in $ts_files
 do
-   ts_out="ts_"`expr 1000 + $index`
-   echo "`basename $ts` -> $ts_out"
-   wget -T 30 -t 10 -q -O $ts_out $ts
+   echo -en "Chunk $index / $nb_files\r"
+   wget -T 30 -t 10 -q -O /tmp/ts_out $ts
+   cat /tmp/ts_out >> $2
    index=`expr $index + 1`
 done
 
-cat ts_1* > $2
-rm -f ts_1* file.m3u8
+echo ""
+rm -f /tmp/ts_out /tmp/file.m3u8
